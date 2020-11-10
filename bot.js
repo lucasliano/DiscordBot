1287f1c61e375b9fff426d27a3d8b45fa276c8eb
Lucas Liaño <lucas.liano2000@gmail.com>
2020-11-10 02:19:55 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,6 +8,7 @@ const client = new Discord.Client();
 
 client.login(Discord_Token);
 
+console.log('-lol ezreal'.slice(0,4) == '-lol')
 
 
 client.on('ready', () => {
@@ -16,6 +17,11 @@ client.on('ready', () => {
 
 client.on('message', msg => {
   if(msg.channel.id == '775510593028751441'){
+    if (msg.content.slice(0,4) == '-lol'){
+      champ = msg.content.slice(5);
+      msg.channel.send('https://las.op.gg/champion/' + champ );
+    }
+
     switch (msg.content){
       case '-ayudameporfi':
         msg.reply('todavía no funciona esto pana \u{1F52B}');
@@ -26,15 +32,32 @@ client.on('message', msg => {
         break;
 
       case '-git':
-        msg.reply('https://github.com/lucasliano/DiscordBot');
+        msg.channel.send('https://github.com/lucasliano/DiscordBot');
         break;
 
       case '-nichis':
-        msg.reply('NIIIIIIIIIICHIIIIIIIIIIIIIIIIIS \u{1F3F3}\u{FE0F}\u{200D}\u{1F308}');
+        msg.channel.send('NIIIIIIIIIICHIIIIIIIIIIIIIIIIIS \u{1F3F3}\u{FE0F}\u{200D}\u{1F308}');
+        break;
+
+      case '-Lourdes':
+        msg.channel.send('Lourdes: te amo pity! \u{2764}');
+        break;
+
+      case '-counters de palu':
+        msg.channel.send('El counter de Palu es jugar sin miedo!')
         break;
 
       case '-pato':
-        msg.reply('me la como y que?');
+        msg.channel.send('Pato: me la como y que?');
+        break;
+
+      case '-chichi':
+        msg.channel.send('CHICHI DEJA DE SER TAN TROLO LA PUTA MADRE \u{1F3F3}\u{FE0F}\u{200D}\u{1F308}');
+        break;
+
+      case '-naked':
+        msg.reply('Te caes a pedazos');
+        break;
     // IMPORTANTE NO PONER NADA EN DEFAULT: !!! SINO EXPLOTA EL SERVER.
     }
   }
