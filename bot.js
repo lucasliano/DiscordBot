f751889e23f186519c80180ed4caae87c0044368
Lucas Liaño <lucas.liano2000@gmail.com>
2020-11-09 21:33:18 -0300

    Inicialización

--- bot.js
+++ bot.js
@@ -1,1 +1,28 @@
-console.log('Hello World! ')
+console.log('Initializing! \u{1F63C}')
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+
+  switch (msg.content){
+    case '-ping':
+      msg.reply('Pong!');
+
+    case '-git':
+      msg.reply('');
+
+    case '-':
+      msg.reply('Wachin, tenes que especificar algo pana')
+
+  }
+
+  }
+});
+
+client.login('Nzc1NDkyNDU3Nzc3MDcwMTQw.X6nHoQ.MtdpRnUD3cPVOXaxyvggT6Q-eG4');
