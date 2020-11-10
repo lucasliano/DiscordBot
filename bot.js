1348ad0abd88d0c593b73a8a8f84c8316637d1bd
Lucas Liaño <lucas.liano2000@gmail.com>
2020-11-09 22:06:46 -0300

    Reupload

--- bot.js
+++ bot.js
@@ -1,28 +1,34 @@
-console.log('Initializing! \u{1F63C}')
+console.log('Initializing!')
+require('dotenv').config();
+
+console.log(process.env)
+
+const Discord_Token = process.env.DISCORD_TOKEN;
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
+
+
+client.login(Discord_Token);
+
+
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-
   switch (msg.content){
-    case '-ping':
-      msg.reply('Pong!');
+    case '-':
+      msg.reply('tenes que especificar algo pana \u{1F52B}');
+
+    case '-hi':
+      msg.reply('Hello!');
 
     case '-git':
-      msg.reply('');
+      msg.reply('https://github.com/lucasliano/DiscordBot');
 
-    case '-':
-      msg.reply('Wachin, tenes que especificar algo pana')
 
-  }
-
+  // IMPORTANTE NO PONER NADA EN DEFAULT: !!! SINO EXPLOTA EL SERVER.
   }
 });
-
-client.login('Nzc1NDkyNDU3Nzc3MDcwMTQw.X6nHoQ.MtdpRnUD3cPVOXaxyvggT6Q-eG4');
