fd0176da7525a02d559ad57b7d3d6d9ded43e443
Lucas Liaño <lucas.liano2000@gmail.com>
2020-11-10 23:00:50 -0300

    Probando funcionalidad

--- git.sh
+++
@@ -1,8 +0,0 @@
-# Fetch the newest code
-git fetch origin master
-
-# Hard reset
-git reset --hard origin/master
-
-# Force pull
-git pull origin master --force
--- svFuncs.js
+++ svFuncs.js
@@ -5,10 +5,13 @@ module.exports = {
   {
     try{
       cmd.run('chmod 777 git.sh'); /* :/ Fix no perms after updating */
-      cmd.get('./git.sh', (err, data) => {  // Run our script
-        if (data) console.log(data);
-        if (err) console.log(err);
-      });
+      cmd.run('./git.sh');
+      // cmd.get('git.sh', (err, data) => {  // Run our script
+      //   if (data) console.log(data);
+      //   if (err) console.log(err);
+      // });
+
+
       cmd.run('refresh');  // Refresh project
 
       console.log("> [GIT] Updated with origin/master");
