1db1632be34dc29ab1488a5d97652e121456c2f9
lucasliano <lliano@est.frba.utn.edu.ar>
2020-11-15 22:54:01 -0300

    First Commit here

--- start.js
+++ start.js
@@ -4,21 +4,6 @@ const path = require('path');
 const bot = require("./src/bot.js");
 const svFuncs = require("./svFuncs.js");
 
-
-const Pool = require('worker-threads-pool')
-
-// const pool = new Pool({max: 5})
-//
-// for (let i = 0; i < 100; i++) {
-//   pool.acquire('/my/worker.js', function (err, worker) {
-//     if (err) throw err
-//     console.log(`started worker ${i} (pool size: ${pool.size})`)
-//     worker.on('exit', function () {
-//       console.log(`worker ${i} exited (pool size: ${pool.size})`)
-//     })
-//   })
-// }
-
 // We try to execute the bot app.
 try
 {
